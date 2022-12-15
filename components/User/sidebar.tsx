2b0b70d9ed2ee75dc1aae15f5cfcb1932460d766
SebLrx <sebastien.leyrissoux@my-digital-school.org>
2022-12-15 08:35:47 +0100

    changements sidebar

--- components/User/sidebar.tsx
+++ components/User/sidebar.tsx
@@ -10,45 +10,74 @@ export default function Sidebar(): JSX.Element {
         <>
             <nav>
                 <ol>
-                    <li className="container mx-auto flex flex-row">
-                        <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
-                            {/* profile picture */}
-                            <Image
-                                src={profilePick}
-                                alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
-                                className="bg-cover"
-                                priority={true}
-                            />
-                        </div>
-                        {/* name */}
-                        <p className="text-lg">Lorem, ipsum.</p>
-                    </li>
-                    <li className="container mx-auto flex flex-row">
-                        <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
-                            {/* profile picture */}
-                            <Image
-                                src={profilePick}
-                                alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
-                                className="bg-cover"
-                                priority={true}
-                            />
-                        </div>
-                        {/* name */}
-                        <p className="text-lg">Lorem, ipsum.</p>
-                    </li>
-                    <li className="container mx-auto flex flex-row">
-                        <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
-                            {/* profile picture */}
-                            <Image
-                                src={profilePick}
-                                alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
-                                className="bg-cover"
-                                priority={true}
-                            />
-                        </div>
-                        {/* name */}
-                        <p className="text-lg">Lorem, ipsum.</p>
-                    </li>
+                    <a>
+                        <li className="container py-3 px-4 mx-auto flex flex-row">
+                            <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
+                                {/* profile picture */}
+                                <Image
+                                    src={profilePick}
+                                    alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
+                                    className="bg-cover"
+                                    priority={true}
+                                />
+                            </div>
+                            <div>
+                                {/* name */}
+                                <p className="text-lg py-6">Lorem, ipsum.</p>
+                            </div>
+                        </li>
+                    </a>
+                    <a>
+                        <li className="container py-3 px-4 mx-auto flex flex-row">
+                            <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
+                                {/* profile picture */}
+                                <Image
+                                    src={profilePick}
+                                    alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
+                                    className="bg-cover"
+                                    priority={true}
+                                />
+                            </div>
+                            <div>
+                                {/* name */}
+                                <p className="text-lg py-6">Lorem, ipsum.</p>
+                            </div>
+                        </li>
+                    </a>
+                    <a>
+                        <li className="container py-3 px-4 mx-auto flex flex-row">
+                            <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
+                                {/* profile picture */}
+                                <Image
+                                    src={profilePick}
+                                    alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
+                                    className="bg-cover"
+                                    priority={true}
+                                />
+                            </div>
+                            <div>
+                                {/* name */}
+                                <p className="text-lg py-6">Lorem, ipsum.</p>
+                            </div>
+                        </li>
+                    </a>
+                    <a>
+                        <li className="container py-3 px-4 mx-auto flex flex-row">
+                            <div className="relative m-1 h-18 w-20 bg-black rounded-full overflow-hidden">
+                                {/* profile picture */}
+                                <Image
+                                    src={profilePick}
+                                    alt="Aerial shot of a beautiful tree in a forest covered with fog in Bled, Slovenia"
+                                    className="bg-cover"
+                                    priority={true}
+                                />
+                            </div>
+                            <div>
+                                {/* name */}
+                                <p className="text-lg py-6">Lorem, ipsum.</p>
+                            </div>
+                        </li>
+                    </a>
                 </ol>
             </nav>
         </>
