f972790f31663fad315715bb0235fd688795ad20
SebLrx <sebastien.leyrissoux@my-digital-school.org>
2022-12-16 08:53:34 +0100

    début back et interface

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -2,22 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import connectDB from "@/lib/database";
 
-import Sections from "@/interfaces/Sections";
+import UserData from "@/interfaces/Sections";
 
-export default async function getHomeData(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  type SectionInfo = Pick<Sections, "title">; // Pick the title property from the Sections interface
+export default async function createUser(req: NextApiRequest, res: NextApiResponse) {
   const conn = connectDB(); // Retreive the inforamtion to be able to connect to the database
   let result: any;
 
   try {
     await conn.connect(); // Connect to the database
 
-    const database = conn.db("portfolio"); // Select the database to use
-    const sections = database.collection<SectionInfo>("sections"); // Select the collection (table)
-    result = await sections.find<SectionInfo>({}).toArray(); // Find all documents in the collection
+    const database = conn.db("projet-blog"); // Select the database to use
+    const sections = database.collection("user"); // Select the collection (table)
+    result = await sections.insertOne({
+      
+    })
   } catch (error) {
     conn.close(); // Close the connection to the database
     console.log(error); // Log the error
